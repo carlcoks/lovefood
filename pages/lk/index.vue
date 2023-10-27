d4be7d0443212579143f5b367d5df56792d6f7ab
Carlcox <ittruemrdro4@yandex.ru>
2023-10-27 20:20:42 +0500

    update

--- pages/lk/index.vue
+++ pages/lk/index.vue
@@ -93,6 +93,91 @@
         </div>
       </div>
     </form>
+
+    <div class="page-lk-personal-addresses">
+      <div class="page-lk-personal-addresses__header">
+        <p class="page-lk-personal-addresses__titl">
+          Сохраненные адреса
+        </p>
+
+        <UIButton
+          color="yellow"
+          class="page-lk-personal-addresses__button"
+        >
+          <UIIcon name="add" />
+          Добавить новый адрес
+        </UIButton>
+      </div>
+
+      <div class="page-lk-personal-addresses__list">
+        <div class="page-lk-personal-address">
+          <div class="page-lk-personal-address__icon">
+            <UIIcon name="house" />
+          </div>
+          <div class="page-lk-personal-address__content">
+            <div class="page-lk-personal-address__info">
+              <p class="page-lk-personal-address__name">
+                Дом
+              </p>
+              <p class="page-lk-personal-address__address">
+                г. Москва, Ул. Стандартная, 21к1
+              </p>
+            </div>
+            <a
+              href="#"
+              class="page-lk-personal-address__edit"
+              @click.prevent=""
+            >
+              <UIIcon name="pencil" />
+            </a>
+          </div>
+        </div>
+        <div class="page-lk-personal-address">
+          <div class="page-lk-personal-address__icon">
+            <UIIcon name="briefcase" />
+          </div>
+          <div class="page-lk-personal-address__content">
+            <div class="page-lk-personal-address__info">
+              <p class="page-lk-personal-address__name">
+                Работа
+              </p>
+              <p class="page-lk-personal-address__address">
+                г. Москва, Ул. Стандартная, 21к1
+              </p>
+            </div>
+            <a
+              href="#"
+              class="page-lk-personal-address__edit"
+              @click.prevent=""
+            >
+              <UIIcon name="pencil" />
+            </a>
+          </div>
+        </div>
+        <div class="page-lk-personal-address">
+          <div class="page-lk-personal-address__icon">
+            <UIIcon name="metka" />
+          </div>
+          <div class="page-lk-personal-address__content">
+            <div class="page-lk-personal-address__info">
+              <p class="page-lk-personal-address__name">
+                Ул. Стандартная, 21к1
+              </p>
+              <p class="page-lk-personal-address__address">
+                г. Москва
+              </p>
+            </div>
+            <a
+              href="#"
+              class="page-lk-personal-address__edit"
+              @click.prevent=""
+            >
+              <UIIcon name="pencil" />
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -150,6 +235,13 @@
 
     top: 16px;
     right: 15px;
+
+    width: 16px;
+    height: 16px;
+
+    ::v-deep svg path {
+      fill: #262626;
+    }
   }
 }
 
@@ -200,6 +292,77 @@
     
     font-weight: 500;
 
+    color: $grayText;
+  }
+}
+
+.page-lk-personal-addresses {
+  display: flex;
+  flex-direction: column;
+  grid-gap: 30px;
+
+  &__header {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+  }
+
+  &__title {
+    @include text_big;
+    font-weight: 600;
+  }
+
+  &__button {
+    font-weight: 500;
+  }
+
+  &__list {
+    display: flex;
+    flex-direction: column;
+    grid-gap: 4px;
+  }
+}
+
+.page-lk-personal-address {
+  display: grid;
+  align-items: center;
+  grid-template-columns: 40px 1fr;
+  grid-gap: 16px;
+
+  &__icon {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+
+  &__content {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+
+    grid-gap: 12px;
+
+    padding: 12px 0;
+
+    border-bottom: 1px solid $grayText2;
+  }
+
+  &__info {
+    display: flex;
+    flex-direction: column;
+    grid-gap: 4px;
+  }
+
+  &__name {
+    @include overflow-text;
+    @include text_normal;
+    font-weight: 600;
+  }
+
+  &__address {
+    @include overflow-text;
+    @include text_small;
+    font-weight: 500;
     color: $grayText;
   }
 }
